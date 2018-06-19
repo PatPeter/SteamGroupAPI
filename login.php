c7455d26d733e945447fd652022934b085911205
Nicholas Solin <solin.nick@gmail.com>
2018-06-18 20:21:31 -0500

    Use Composer for phpseclib instead, and migrate to 2.0.11

--- login.php
+++ login.php
@@ -1,67 +1,84 @@
 <?php
-include('phpseclib/Math/BigInteger.php');    
-include('phpseclib/Crypt/RSA.php');
-define('CRYPT_RSA_PKCS15_COMPAT', true);  // May not be necessary, but never hurts to be sure.
-
-$url_rsa = 'https://steamcommunity.com/login/getrsakey/?username=';
-$username = "";  // Insert bot's username here.
-$password = "";  // Insert bot's password here.
-
-
-// Skip the extra work of POST'ing the data, just GET'ing the url works fine and saves space
-$result = file_get_contents($url_rsa . $username);
-$result = json_decode($result);
-
-/*if ($result->success){
-    echo "Got Info!<br/><br/>";
-} else if (!$result->success){            // Remove comment markers during testing
-    echo "Unable to grab Info<br/><br/>"; // You can also use this to log errors to a database or email you if needed.
-}*/
-
-//echo var_dump($result);
-// More testing code, just to help you see what's going on.
-//echo "<br/><br/>";
-
-$rsa = new Crypt_RSA();
-$rsa->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
-$key = array(
-    'n' => new Math_BigInteger($result->publickey_mod,16),
-    'e' => new Math_BigInteger($result->publickey_exp,16) // Fixed base :)
-);
-$rsa->loadKey($key);
-$password = base64_encode($rsa->encrypt($password)); // Steam uses Base64_Encode()
-
-echo "Password Encrypted: " . $password . "<br/><br/>";
-// Should look like numbers and letters, any 'weird' characters shouldn't be here
-
-$data = array(
-    'username' => $username,
-    'password' => $password,
-    'twofactorcode'=> "",
-    'emailauth'=> "", 
-    'loginfriendlyname'=> "", 
-    'captchagid'=> "",         // If all goes well, you shouldn't need to worry
-    'captcha_text'=> "",       // about Captcha.
-    'emailsteamid'=> "", 
-    'rsatimestamp'=> $result->timestamp, 
-    'remember_login'=> "false" 
-);
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data),
-    ),
-);
-var_dump($options);
-
-$url="https://steamcommunity.com/login/dologin/";
-
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-$result = json_decode($result);
-
-echo var_dump($result);
-// Hopefully everything went O.K. and you should be able pull out tokens
-// for usage in your application.
+require_once 'vendor/autoload.php';
+use \Curl\Curl;
+use \phpseclib\Crypt\RSA;
+use \phpseclib\Math\BigInteger;
+function login($username, $password) {
+	// If username or password aren't set then return false
+	if (!isset($username) || !isset($password))	return false;
+	// Set variables
+	$donotcache = round(microtime(true)*1000);
+	// Define class Curl() in variable $curl
+	$curl = new Curl();
+	// Define class Crypt_RSA in variable $rsa
+	$rsa = new RSA();
+	// Retrieve RSA key
+	$curl->post('https://steamcommunity.com/login/getrsakey/', array(
+		'donotcache'	=>	$donotcache,
+		'username'		=>	$username
+	));
+	// If RSA key failed then return false
+	if ($curl->response->success != true) return false;
+	// Get timestamp
+	$rsatimestamp = $curl->response->timestamp;
+	// Encrypt password
+	// Set keys
+	$key = array(
+		'n' => new BigInteger($curl->response->publickey_mod,16),
+		'e' => new BigInteger($curl->response->publickey_exp,16)
+	);
+	// Define exponent
+	define('CRYPT_RSA_EXPONENT', 010001);
+	// Load the key
+	if (!$rsa->loadKey($key)) return false;
+	// Set settings
+	$rsa->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
+	$rsa->setHash('sha256');
+	// Encrypt password
+	$encrypted_password = base64_encode($rsa->encrypt($password));
+	// Login to Steam
+	$curl->post('https://steamcommunity.com/login/dologin/', array(
+		'donotcache'	=>	$donotcache,
+		'password'		=>	$encrypted_password,
+		'username'		=>	$username,
+		'rsatimestamp'	=>	$rsatimestamp
+	));
+	// If login failed then return false
+	if ($curl->response->success != true) return false;
+	// Setup transfer variables
+	$steamid		=	$curl->response->transfer_parameters->steamid;
+	$token 			=	$curl->response->transfer_parameters->token;
+	$auth			=	$curl->response->transfer_parameters->auth;
+	$token_secure	=	$curl->response->transfer_parameters->token_secure;
+	// Transfer 1
+	$curl->post('https://store.steampowered.com/login/transfer', array(
+		'steamid'			=>	$steamid,
+		'token'				=>	$token,
+		'auth'				=>	$auth,
+		'remember_login'	=>	true,
+		'token_secure'		=>	$token_secure
+	));
+	
+	// If transfer 1 failed then return false
+	//if (!strpos($curl->response, 'transfer_success')) return false;
+	// Transfer broken? Don't need it anyway.
+	// Transfer 2
+	$curl->post('https://help.steampowered.com/login/transfer', array(
+		'steamid'			=>	$steamid,
+		'token'				=>	$token,
+		'auth'				=>	$auth,
+		'remember_login'	=>	true,
+		'token_secure'		=>	$token_secure
+	));
+	// If transfer 2 failed then return false
+	//if (!strpos($curl->response, 'transfer_success')) return false;
+	// Transfer broken? Don't need it anyway.
+	// Close connection
+	$curl->close();
+	// If all else was a success, return true
+	return true;
+}
+function is_logged(){
+	return ($curl->get('http://steamcommunity.com/actions/GetNotificationCounts') != "null");
+}
 ?>
